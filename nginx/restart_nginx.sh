d157ff15c32a140005c607a62b205c3199eee50c
Archit <architgarg.ai@gmail.com>
2023-01-03 14:54:22 +0530

    updating restart_nginx.sh

--- nginx/restart_nginx.sh
+++ nginx/restart_nginx.sh
@@ -4,4 +4,4 @@ sudo rsync -avh /home/deployer/ema/nginx/ /etc/nginx/
 cd /etc/nginx/sites-enabled
 sudo rm *
 sudo ln -sf ../sites-available/* .
-sudo systemctl restart nginx
+sudo systemctl restart nginx.service
